69f584042e5ffce4caff989dd5ce54274cfd6536
Quit108 <emontingelli@gmail.com>
2026-01-11 12:49:35 -0500

    Control Flow Lab

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,46 @@
+let userRole = "admin";
+let accessLevel;
+
+if (userRole == "admin") {
+    accessLevel = "Full Access Granted";
+} else if (userRole == "manager") {
+    accessLevel = "Limited Access Granted";
+} else {
+    accessLevel = "No Access Granted";
+}
+console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+    if (userRole == "admin") {
+        userMessage = "Welcome, Admin!";
+    } else {
+        userMessage = "Welcome, User!";
+
+    }
+} else {
+    userMessage = "Please Log In to Access The System.";
+}
+
+console.log("User Message:", userMessage)
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default:
+        userCategory = "Uknown";
+}
+
+console.log("User Category:", userCategory);
