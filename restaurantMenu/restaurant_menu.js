2557a8ab7b1ecbe02491a0cbee5852d35f491848
Quit108 <emontingelli@gmail.com>
2026-01-18 10:38:42 -0500

    Added pricing to restaurantMenu

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pancakes- $12', 'Eggs Benedict- $22.99', 'Oatmeal- $21.99', 'Frittata- $15'];
+const mainCourseMenu = ['Steak- $25.99', 'Pasta- $21.99', 'Burger- $29.99', 'Salmon- $35'];
+const dessertMenu = ['Cake- $12', 'Ice Cream- $10.99', 'Pudding- $13.99', 'Fruit Salad- $8.99'];
 
 const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Item ${index + 1}: ${item}</p>`).join('');
 document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
