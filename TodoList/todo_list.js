ae56a24512efb5a897fbe8580dba88c3f1374df2
Quit108 <emontingelli@gmail.com>
2026-01-31 15:05:34 -0500

    fixed checkbox

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -17,7 +17,7 @@ function displayTasks () {
     taskList.innerHTML = '';
     tasks.forEach((task, index) => {
         const li  = document.createElement('li');
-        li.innerHTML = `<input type="checkbox" id="task-${index} ${task.completed} ? "checked" : ""}>
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
             <label for="task-${index}">${task.text}</label>`
         li.querySelector('input').addEventListener('change', () => toggleTask(index));
         taskList.appendChild(li);
