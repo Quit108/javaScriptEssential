81ed77290e1daa09625bf03f5d09a92dc8e33cdd
Quit108 <emontingelli@gmail.com>
2026-02-25 10:38:08 -0500

    added latlongForm function

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -20,4 +20,28 @@ function showWeatherDetails(event) {
         });
 }
 
+function latLonWeather(event) {
+    event.preventDefault();
+    
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
+    const apiKey = 'YOUR_API_KEY' // <- insert API KEY here
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+                <h2>Weather at Lat: ${lat} and Lon: ${lon}</h2>
+                <p>Temperature: ${data.main.temp} &#8451;</p>
+                <p>Weather: ${data.weather[0].description}</p>`;
+        })
+        .catch(error => {
+            console.error('Error Fetching Weather:', error);
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `<p>Failed to fetch weather. Please try again.</p>`;
+        });
+}
 document.getElementById('weatherForm').addEventListener('submit', showWeatherDetails );
+document.getElementById('latLonForm').addEventListener('submit', latLonWeather)
